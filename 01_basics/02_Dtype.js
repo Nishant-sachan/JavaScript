d471956cb61522aa3207a57a8134050762de6ca9
Nishant Sachan <mr.nishantsachan@gmail.com>
2025-08-22 13:38:01 +0000

    new config

---
+++ 01_basics/02_Dtype.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as per new javaScript version 
+// alert ("hello ") // not allowed  because of we are using node js not browser
+
+// Documentation -- mdn and ecmascript 
+
+let name = "Nishant"  // string 
+let age = 20          // Number   range 2^53   , bigint is also available 
+let loggedIn= false   // Bool 
+
+//null= standalone value
+//bool = true/false
+//undefined = value not defined yet 
+//symbol = Unique 
+// object 
+
+console.table([typeof  name, typeof age , typeof loggedIn ]);
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+let score = true 
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* Type      output 
+   "33"      number 
+   "33abc"    NaN
+   "Nishant"  NaN
+   true       1
+   false      0
+   null       0
+   undefined  NaN  
+   */
